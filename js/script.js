33fa3eae7c4444b1dfaff7b7edd2600e846734fa
Brad Whipp <contactbradly@gmail.com>
2022-02-18 21:08:10 -0800

    I think I am actually done but all the JS I wrote does nothing and I dont know why

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const guessedLettersElement = document.querySelector(".guessed-letters");
-const guessletterButton = document.querySelector(".guess");
+const guessLetterButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
@@ -7,20 +7,31 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
-const guessedLetters = [];
+let word = "magnolia";
+let guessedLetters = [];
+let remainingGuesses = 8;
 
 //display our symbols as placeholders  for the chosen word's letters?
+const getWord = async function () {
+  const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+  const words = await response.text();
+  const wordArray = words.split("\n");
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  word = wordArray[randomIndex].trim();
+  placeholder(word);
+};
+
+//fire off the game?
+getWord();
+  //Display the symbols? But my page is still not working!!!
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
     console.log(letter);
-    placeholderLetters.push("🟣");
+    placeholderLetters.push("🟢");
   }
 wordInprogress.innerText = placeholderLetters.join("");
 };
-
-placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
@@ -63,10 +74,21 @@ const makeGuess = function (guess) {
   } else {
     guessedLetters.push(guess);
     console.log(guessedLetters);
+    updateGuessesRemaining(guess);
     showGuessedLetters();
     updateWordInProgress(guessedLetters);
   }
 };
+
+const showguessedLetters = function () {
+  //clear list first
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
+
 
 const updateWordInProgress = function (guessedLetters) {
   const wordUpper = word.toUpperCase();
@@ -76,7 +98,7 @@ const updateWordInProgress = function (guessedLetters) {
     if (guessedLetters.includes(letter)) {
       revealWord.push(letter.toUpperCase());
     } else {
-      revealWord.push("🟣");
+      revealWord.push("🟢");
     }
   }
    //console.log(revealWord);
@@ -107,5 +129,31 @@ const updateWordInProgress = function (guessedLetters) {
    if (word.toUpperCase() === wordInProgress.innerText) {
      message.classList.add("win");
      message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+
+     startOver();
    }
  };
+
+const startOver = function () {
+  guessLetterButton.classList.add("hide");
+  remainingGuessesElement.classList.add("hide");
+  guessedLettersElement.classList.add("hide");
+  platAgainButton.classList.remove("hide");
+};
+
+playAgainButton.addEventListener("click", function () {
+  //reset all originalvalues - grab new words
+  message.classList.remove("win");
+  guessedLetters = [];
+  remainingGuesses = 8;
+  remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+  guessedLettersElement.innerHTML = "";
+  message.innerText = "";
+  //new words
+
+ //show the right UI elements? mine still isnt weorking!!!!!
+ guessedLetterButton.classList.remove("hide");
+ playAgainButton.classList.add("hide");
+ remainingGuessesElement.classList.remove("hide");
+ guessedLettersElement.classList.remove("hide");
+});
